686956a2cb6a7da6fae1d73532d5ba4a6287ede2
CemBeast <cem.beyenal@wsu.edu>
2025-02-24 22:24:33 -0800

    Adding leetcode 217 in c++

---
+++ C++/217ContainsDuplicates.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        Std::unordered_map<int, int> m;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            m[nums[i]]++;
+            if(m[nums[i]] > 1)
+                return true;
+        }
+        return false;
+    }
+};
