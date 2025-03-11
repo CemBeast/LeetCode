178192f80e21734fe07886cf59068482f2835f04
CemBeast <cem.beyenal@wsu.edu>
2025-03-11 15:45:54 -0700

    Adding leetcode 167 in c++ with sliding window technique

---
+++ C++/167TwoSumII-InputApprayIsSorted.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0, right = numbers.size() - 1;
+        vector<int> ans;
+        while (left <= right)
+        {
+            if (numbers[left] + numbers[right] == target)
+            {
+                ans.push_back(left + 1);
+                ans.push_back(right + 1);
+                break;
+            }
+            if (numbers[left] + numbers[right] < target)
+            {
+                left++;
+            }
+            if (numbers[left] + numbers[right] > target)
+            {
+                right--;
+            }
+        }
+        return ans;
+    }
+};
