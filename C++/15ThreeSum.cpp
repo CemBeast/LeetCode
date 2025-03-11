2f582d8b6576ca30165787ea71353e94342b12ba
CemBeast <cem.beyenal@wsu.edu>
2025-03-11 16:06:43 -0700

    adding leetcode 15 using a sliding window technique

---
+++ C++/15ThreeSum.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] > 0)
+                break;
+            else if (i > 0 && nums[i] == nums[i-1])
+                continue;
+            int low = i + 1, high = n - 1;
+            while(low < high)
+            {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == 0)
+                {
+                    ans.push_back({nums[i], nums[low], nums[high]});
+                    low++;
+                    high--;
+                    while(low < high && nums[low] == nums[low-1])
+                    {
+                        low++;
+                    }
+                    while(low < high && nums[high] == nums[high+1])
+                        high--;
+                }
+                else if (sum < 0)
+                    low++;
+                else
+                    high--;
+            }
+        }
+        return ans;
+    }
+};
