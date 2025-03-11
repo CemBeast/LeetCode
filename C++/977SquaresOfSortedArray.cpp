5269fbc54e834c49305166c4946e169c8b75a1bf
CemBeast <cem.beyenal@wsu.edu>
2025-03-11 15:34:50 -0700

    adding leetcode 977 in c++

---
+++ C++/977SquaresOfSortedArray.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            res.push_back(pow(nums[i], 2));
+        }
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
