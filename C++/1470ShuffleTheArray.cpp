94788ffe59ff744aa1a54d6d73a12d5c90f58e48
CemBeast <cem.beyenal@wsu.edu>
2025-11-08 21:31:40 -0800

    leet

--- C++/1470ShuffleTheArray.cpp
+++ C++/1470ShuffleTheArray.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans;
+        vector<int> res;
         for(int i = 0; i < n; i++){
-            ans.push_back(nums[i]);
-            ans.push_back(nums[n + i]);
+            res.push_back(nums[i]);
+            res.push_back(nums[n + i]);
         }
-        return ans;
+        return res;
     }
 };
