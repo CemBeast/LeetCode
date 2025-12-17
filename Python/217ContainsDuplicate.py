0da83ad5b7f57af8fa77fe7a18c943fb4d3083c4
CemBeast <cem.beyenal@wsu.edu>
2025-12-16 16:16:10 -0800

    contains duplicate in python

---
+++ Python/217ContainsDuplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        m = {}
+        for num in nums:
+            if num in m:
+                return True
+            m[num] = 1
+        return False
