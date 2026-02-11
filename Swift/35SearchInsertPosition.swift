6620a75add654f6e2b3f5a702e0100f5d1b36eb1
CemBeast <cem.beyenal@wsu.edu>
2026-02-11 10:00:30 -0800

    leet 35 in swift

---
+++ Swift/35SearchInsertPosition.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0, right = nums.count - 1
+        while left <= right {
+            let mid = left + (right - left) / 2
+            if target == nums[mid] {
+                return mid
+            }
+            else if target < nums[mid] {
+                right = mid - 1
+            }
+            else {
+                left = mid + 1
+            }
+        }
+        return left
+    }
+}
